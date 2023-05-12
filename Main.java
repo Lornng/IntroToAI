be50b4474e67ab1dcca98edf75c6c1ea4cb0b78b
Lornng <ngjianjia02@gmail.com>
2023-05-13 00:16:47 +1000

    text file

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args){
-        String filename = "C:/Users/chngc/OneDrive/Desktop/INTRO_TO_AI/Assignment_2/test_HornKB.txt";
+        String filename = "test_HornKB.txt";
         KnowledgeBase KB = new KnowledgeBase(filename);
         TT tt = new TT();
 
