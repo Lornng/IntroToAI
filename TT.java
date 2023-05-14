d543f349986e0172daf5565b801d27853467a556
Lornng <ngjianjia02@gmail.com>
2023-05-15 01:26:31 +1000

    changes

--- TT.java
+++ TT.java
@@ -1,9 +1,11 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TT {
     HashSet<String> symbols;
-    HashMap<String, Boolean> model = new HashMap<>();
+    HashMap<String, Boolean> symbolMap = new HashMap<>();
     Boolean[][] TT;
     String query;
 
@@ -22,25 +24,40 @@ public class TT {
 
         TT = new Boolean[rows][numSymbols];
 
+        // Create a list of symbols to iterate over (to maintain order)
+        List<String> symbolList = new ArrayList<>(symbols);
+        
         for(int row = 0; row < rows; row++){
             for(int col = 0; col < numSymbols; col++){
+                String symbolString = symbolList.get(col);
                 //calculate the truth value for the current variable 
                 boolean truthValue = ((row >> (numSymbols - col - 1)) & 1) == 1;
 
                 TT[row][col] = truthValue;
 
-                if(TT[row][col] == true){
-                    System.out.print("T");
-                }else if (TT[row][col] == false){
-                    System.out.print("F");
-                }
+                // if(TT[row][col] == true){
+                //     System.out.print("T");
+                // }else if (TT[row][col] == false){
+                //     System.out.print("F");
+                // }
+
+                //assign truthValue to the symbol in the map
+                symbolMap.put(symbolString + "[" + row + "]", truthValue);
+
+                // boolean symbolValue = symbolMap.get(symbolString);
+                // System.out.println(symbolString + ": ");
+                // System.out.println(symbolValue ? "T" : "F");
+
             }
             System.out.println("");
+            ValidateValue();
         }
+        System.out.println(symbolList);
+        System.out.println(symbolMap.get("h[2046]"));
     }
 
-    public void findTrueConfigs(){
-        boolean 
+    public void ValidateValue(){
+
     }
 
     // public boolean TT_CHECK_ALL(String kb, String query, HashSet<String> symbols, HashMap<String, Boolean> model){
